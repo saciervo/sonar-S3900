65f37dc4b681a6e2fc062bef1b69f2fe9f13c5f5
Sandro Ciervo <sandro.ciervo@neolution.ch>
2019-08-16 13:15:44 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace ValidateParameterProblemS3900
+{
+    using System;
+    using System.IO;
+
+    public class Class1
+    {
+        public string Foo(string name)
+        {
+            if (name == null)
+            {
+                name = Guid.NewGuid().ToString("N");
+            }
+
+            return string.Join("_", name.Split(Path.GetInvalidFileNameChars()));
+        }
+    }
+}
